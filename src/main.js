a6afc65f8fa76a78c8623047153ba20aedd30b3f
Andre Luiz <andre.paraguai@gmail.com>
2026-01-12 16:31:02 -0300

    Seguindo e copiando a aula, entendendo 1%

--- src/main.js
+++ src/main.js
@@ -11,3 +11,6 @@ import "./styles/schedule.css"
 // js
 import "./modules/form/submit.js"
 import "./modules/page-load.js"
+import "./modules/form/date-change.js"
+
+
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
 const form = document.querySelector('form');
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
+
 
 //data atual para formatar o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
@@ -13,5 +15,39 @@ selectedDate.min = inputToday
 form.onsubmit = (event) => {
   //previne o comportamento padrão de envio do formulário
   event.preventDefault();
-  console.log ("enviado")
+
+  try {
+    //recuperando o nome do cliente
+    const name = clientName.value.trim()
+    if (!name) {
+      return alert("Informe o seu nome para realizar o agendamento.")
+    }
+
+    //recupera o horario
+    const hourSelected = document.querySelector('.hour-selected')
+
+    if (!hourSelected) {
+      return alert("Selecione um horário.")
+    }
+
+    //recupera somente a hora 
+    const [hour] = hourSelected.innerText.split(":")
+    
+    //inserir a hora na data
+    const when = dayjs(selectedDate.value).add(hour,"hour")
+    
+    //gera um ID
+
+    const id = new Date().getTime()
+
+    console.log ({
+      id,
+      name,
+      when,
+    })
+
+  } catch (error) {
+    alert("Não foi possivel realizar o agendamento")
+    console.log(error)
+  }
 }
