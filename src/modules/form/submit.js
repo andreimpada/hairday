593be47db9a078647dacec0cbdce0929cd5cfdb4
Andre Luiz <andre.paraguai@gmail.com>
2026-01-13 16:38:22 -0300

    aula

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,7 @@
 import dayjs from 'dayjs';
+
+import {scheduleNew} from "../../services/schedule-new.js"
+
 const form = document.querySelector('form');
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -12,7 +15,7 @@ selectedDate.value = inputToday
 selectedDate.min = inputToday
 
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   //previne o comportamento padrão de envio do formulário
   event.preventDefault();
 
@@ -40,7 +43,7 @@ form.onsubmit = (event) => {
 
     const id = new Date().getTime()
 
-    console.log ({
+    await scheduleNew ({
       id,
       name,
       when,
